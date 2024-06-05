ef951502775a6a8e9d97c3d87a41dc1cdcc800fe
Shinsuke Saito <s.s.saito108@gmail.com>
2024-06-05 18:55:43 +0900

    オリジナルテーマ読み込み

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 };
 
 const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
+  theme: "@ssaitho/nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "./style.css";
+import "@ssaitho/nextra-theme-docs/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
