7ef77b2a7444c04d89be5793ff91b9d382bc40b2
Shinsuke Saito <s.s.saito108@gmail.com>
2024-06-07 20:46:28 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
-  basePath: '/nextra_sample_ts',
+  basePath: "/nextra_sample_ts",
 };
 
 const withNextra = require("nextra")({
