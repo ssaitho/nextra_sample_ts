d133cfdb3e47ef75ef4a5ef826f3c149577861b9
Shinsuke Saito <s.s.saito108@gmail.com>
2024-06-24 20:43:03 +0900

    basePathだけだとローカル立ち上げた時にエラーになるので、修正

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
-  basePath: "/nextra_sample_ts",
+  basePath: isProd ? "/nextra_sample_ts" : "",
 };
 
 const withNextra = require("nextra")({
