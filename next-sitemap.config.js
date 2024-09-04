e0d8047f0fe7f3ba3c812829b129ace9f961db35
Shinsuke Saito <s.s.saito108@gmail.com>
2024-09-04 13:01:04 +0900

    indexサイトマップを無効

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://ssaitho.github.io/nextra_sample_ts', // 公開するサイトのURL
+  generateIndexSitemap: false, // (オプション) インデックスサイトマップを無効にする場合
   generateRobotsTxt: true, // (オプション) robots.txtも生成する場合
   outDir: './out', // 静的エクスポートの出力ディレクトリに合わせる
 }
